24459fa0d27b6d33d2f460ead12bb05fc63d0b76
Terence LeRoy van Jaarsveldt <vanjaarsveldttl@gmail.com>
2022-02-03 18:30:27 +0200

    Additional fields for users

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,9 @@ interface User{
     uid: string;
     displayName: string;
     email: string;
+    createdDate: Date;
+    lastModifiedDate: Date;
+    createdUser?: User;
     photoURL?: string;
     name?: string;
     surname?: string;
