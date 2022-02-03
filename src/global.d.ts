24d60df1c0287adb3d5a63c9f2b186e48ae3d28d
Terence LeRoy van Jaarsveldt <vanjaarsveldttl@gmail.com>
2022-02-03 18:08:16 +0200

    Add User definition

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="svelte" />
+
+interface User{
+    uid: string;
+    displayName: string;
+    email: string;
+    photoURL?: string;
+    name?: string;
+    surname?: string;
+    contactNumber?: string;
+}
